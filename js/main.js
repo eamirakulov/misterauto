21885cc3e6bd47c38afef4685603103b296231cb
eamirakulov <emil@amirakulov.com>
2019-09-13 22:06:11 +0600

    Add js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+	$('#carousel').slick({
+		arrows: true
+	});
+	$('.autos').slick({
+		arrows: true,
+		slidesToShow: 7
+	});
+	$('.parts').slick({
+		arrows: true,
+		slidesToShow: 7
+	});
+})
